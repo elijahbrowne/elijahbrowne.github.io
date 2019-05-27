84ef5144aa25e2adda3f4e118eef656d47099154
elijahbrowne <elijahbrowne48@gmail.com>
2019-05-27 13:43:49 -0400

    Fixed A LOT of spelling mistakes

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 // set up text to print, each item in array is new line
 var aText = new Array(
   "Elijahswork:", 
-  "One students dream to become a fullstack developer"
+  "One student's dream to become a fullstack developer"
   );
   var iSpeed = 100; // time delay of print out
   var iIndex = 0; // start printing array at this posision
